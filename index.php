4d5c88dc62b4fb9fc56f70e81d7e797a92539a63
Bolton <bolton.otieno@gmail.com>
2018-11-20 10:23:13 +0300

    set up file structure

--- index.php
+++ index.php
@@ -4,16 +4,15 @@
  <head>
      <meta charset="utf-8" />
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <title>Page Title</title>
+     <title>NeoFit</title>
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-     <script src="main.js"></script>
  </head>
  <body>
      
  
 
   <!--JavaScript at end of body for optimized loading-->
-  <script type="text/javascript" src="js/js.js"></script>
+  <script type="text/javascript" src="js/main.js"></script>
   </body>
   </html>

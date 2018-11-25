8b44b61eff4ed5162cc5d75fe5f3a5754bf9abac
Bolton <bolton.otieno@gmail.com>
2018-11-25 19:52:23 +0300

    add and style footer

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(function() {
-    var header = $(".navbar");
+    var header = $(".nav-bar");
   
     $(window).scroll(function() {    
         var scroll = $(window).scrollTop();
         if (scroll >= 50) {
-            header.addClass("scrolled");
+            header.addClass("scrolled-bar");
         } else {
-            header.removeClass("scrolled");
+            header.removeClass("scrolled-bar");
         }
     });
 });
---
+++ pages/footer.php
@@ -0,0 +1,62 @@
+<link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+<footer class="mainfooter" role="contentinfo">
+  <div class="footer-middle">
+  <div class="container">
+    <div class="row">
+      <div class="col s12 l3">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4>Theme Styles</h4>
+          <ul class="list-unstyled">
+            <li><a href="">Extrem Biking</a></li>
+            <li><a href="">Swimming</a></li>
+            <li><a href="">Hiking</a></li>
+            <li><a href="">Nature Walk</a></li>
+            <li><a href="">Ziplining</a></li>
+            <li><a href="">Team Buiding</a></li>
+            <li><a href="">Brigde</a></li>
+            <li><a href="">WildLife</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col s12 l3">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4>About Us</h4>
+          <ul class="list-unstyled">
+              <p>We are tours,adventures & events mgt Co.Dealing also with team buildings for groups & corporate</p>
+          </ul>
+        </div>
+      </div>
+      <div class="col s12 l3">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4>Contact Us</h4>
+          <ul class="list-unstyled">
+            <li><a class="material-icons">call</a> 0712 345 678</li>
+            <li><a class="material-icons">email</a> neofit@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+    	<div class="col s12 l3">
+    		<h4>Follow Us</h4>
+            <ul class="social-network social-circle">
+             <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+             <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+             <li><a href="#" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+             <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+            </ul>				
+		</div>
+    </div>
+	<div class="row">
+		<div class="col-md-12 copy">
+			<p class="text-center">&copy; Copyright 2018 - Neo Fit Adventures.  All rights reserved.</p>
+		</div>
+	</div>
+
+
+  </div>
+  </div>
+</footer>
+

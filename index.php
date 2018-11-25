5817e2679df144ac477e7f4016be0639920ea561
Bolton <bolton.otieno@gmail.com>
2018-11-25 11:37:13 +0300

    style navbar

--- index.php
+++ index.php
@@ -5,14 +5,23 @@
      <meta charset="utf-8" />
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <title>NeoFit</title>
+     <link rel="icon" href='img/icon_black.png' sizes="16x16">
      <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+     <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700">
  </head>
  <body>
      
+    <!--NavBar-->
+        <?php include 'pages/navbar.php' ; ?>
+     <!--End NavBar-->
+
  
 
   <!--JavaScript at end of body for optimized loading-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script type="text/javascript" src="js/main.js"></script>
+
   </body>
   </html>
---
+++ pages/navbar.php
@@ -0,0 +1,43 @@
+  <!-- Nav page -->
+  <section id="frontpage">
+    <div class="navbar">
+      <nav>
+      <div id="logo">
+        <h1>Neo_Fit</h1>
+        <h1>Neo_Fit</h1>
+        <h1>Neo_Fit</h1>
+      </div>
+        <ul class="nav-menu">
+          <li>
+            <a href="#frontpage" class="hover_link">Home</a>
+          </li>
+          <li>
+            <a href="#about" class="hover_link">About</a>
+          </li>
+          <li>
+            <a href="#projects" class="hover_link">Projects</a>
+          </li>
+          <li>
+            <a href="#contact" class="hover_link">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div class="frontpagetext">
+      <h1>Neo Fit Adventures</h1>
+    </div>
+  </section>
+
+  <section id="about">
+    <p>Content here...</p>
+  </section>
+  <section id="projects">
+    <p>Content here...</p>
+  </section>
+  <section id="contact">
+    <p>Content here...</p>
+  </section>
+  
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="./js/main.js"></script>
